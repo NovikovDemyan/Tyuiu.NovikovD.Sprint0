f35a33ea7a6a6a936a4f1f4463621f01c4b8849c
demyan <demyan5002@yandex.ru>
2025-05-18 16:35:15 +0500

    Task5

---
+++ Tyuiu.NovikovD.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.NovikovD.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b) => a + b;
+        public static int Subtraction(int a, int b) => a - b;
+        public static int Multiplication(int a, int b) => a * b;
+        public static int Division(int a, int b) => a / b;
+    }
+}
