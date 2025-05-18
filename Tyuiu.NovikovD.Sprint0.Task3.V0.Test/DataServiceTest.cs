e380d259dacdf0845ca9ceb72eeb77c1a094e53f
demyan <demyan5002@yandex.ru>
2025-05-18 16:16:26 +0500

    Task3

---
+++ Tyuiu.NovikovD.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.NovikovD.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.NovikovD.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckSumValid()
+        {
+            int a = 5;
+            int b = 10;
+            int result = DataService.Sum(a, b);
+            Assert.AreEqual(15, result); // Проверка: 5 + 10 = 15
+        }
+    }
+}
---
+++ Tyuiu.NovikovD.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.NovikovD.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.NovikovD.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 10;
+            Console.WriteLine($"{num1} + {num2} = {DataService.Sum(num1, num2)}");
+            Console.ReadKey();
+        }
+    }
+}
